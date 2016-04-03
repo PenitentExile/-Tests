6872e96ee028bbc03750c0685774ebd77b71ca0f
PenitentExile <phantomhunter@gmail.com>
2016-04-03 09:04:20 -0700

    Added files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int normal = 329821;
+    int reverse = 0;
+    while (normal != 0)
+    {
+        reverse = reverse * 10;
+        reverse = reverse + normal % 10;
+        normal = normal/10;
+
+    }
+    cout << reverse << endl;
+}
+
