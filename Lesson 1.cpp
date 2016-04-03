76ec3370d3ae2b13c8ea18ef5eb10b9420321c6b
PenitentExile <phantomhunter@gmail.com>
2016-04-03 09:08:08 -0700

    Update Lesson 1.cpp

--- Lesson 1.cpp
+++ Lesson 1.cpp
@@ -8,9 +8,9 @@ int main()
     int reverse = 0;
     while (normal != 0)
     {
-        reverse = reverse * 10;
-        reverse = reverse + normal % 10;
-        normal = normal/10;
+        reverse = reverse * 10; // умножаем искомое число на 10, чтобы прибавить к нему разряд
+        reverse = reverse + normal % 10; // записываем в reverse только остаток от деления исходного числа на 10 (последний разряд)
+        normal = normal/10; // делим исходное число на десять, чтобы убрать разряд и гоняем цикл пока в нем не останется разрядов (они все запишутся в качестве остатка от деления в обратное число)
 
     }
     cout << reverse << endl;
