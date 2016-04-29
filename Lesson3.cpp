4bc8116f9d7050120dd707f9bf619830268eec7d
PenitentExile <phantomhunter@gmail.com>
2016-04-29 03:21:42 -0700

    Update and rename main.cpp to Lesson3.cpp

--- Lesson3.cpp
+++ Lesson3.cpp
@@ -2,42 +2,8 @@
 #include <cmath>
 #include <vector>
 #include <cstring>
-#include <array>
 
 using namespace std;
-
-//class NPC
-//{
-//    float centerhead[2], direction[2];
-
-//    public:
-//    void Talk ();
-//    void Move ();
-//};
-
-//class GG : public NPC
-//{
-//public:
-//    GG ();
-//    void Hearing (float centerhead[2], direction[2]);
-//    ~GG;
-//};
-
-//GG:GG ()
-//{
-//  centerhead[2] = {0,0}
-//  direction[2] = {0;0};
-//}
-
-//void Hearing ()
-//{
-//    float headposition, distance, shift;
-//}
-
-//void Angle (float x1, float y1, float x2, float y2)
-//{
-//    float angle_between = (x1*x2 + y1*y2) / (sqrt(pow(x1, 2) + (pow(y1, 2)))) * (sqrt(pow(x2, 2) + (pow(y2, 2))));
-//}
 
   double Fibonacci (double n)
   {
@@ -69,27 +35,6 @@ using namespace std;
       }
   }
 
-  struct students
-  {
-       string name;
-       int ratings[2];
-       string status;
-  };
-
-  string assignStatus (students *classStudents, int size n)
-  {
-      int allRatings = 0;
-      for (int i = 0; i < 2; i++)
-      {
-      students currentRatings = data[i]ratings[0] + data[i]ratings[1];
-      allRatings = allRatings + currentRatings;
-      int cycleCount =
-      }
-      allRatings = allRatings /
-  }
-
-  string findBestWorst
-
 int main()
 {
 //    1. Реализовать вычисление n-го числа Фибоначчи через массив.
@@ -120,32 +65,11 @@ int main()
    }
 
 //    4. Написать рекурсивную функцию по поиску минимальной цифры в числе.
-//   int a;
-//   cout << "Введите целое число: " << endl;
-//   cin >> a;
-//   cout << MinimalNumber (a, 9)  << endl;
+     int a;
+     cout << "Введите целое число: " << endl;
+     cin >> a;
+     cout << MinimalNumber (a, 9)  << endl;
 
 //    5. Реализовать алгоритм поиска любой возрастающей подпоследовательности: 5 1 2 3 7 -2 3 5 -> 1 2 3 7
 //    6. Перевернуть каждый половину массива: 1 2 3 4 5 6 7 8 9 10 -> 5 4 3 2 1 10 9 8 7 6
-
-//    7. Найти среднее арифметическое всех оценок студентов. Найти лучшего студента и самого отстающего. Выдать всех студентов, у которых оценка ниже, чем среднее арифметическое всех. (структуру придумать самим).
-   students data[3] = {{"Vasya", 4, 5}, {"Petya", 5, 5}, {"Sasha", 3, 4}};
-   for (int i = 0; i < 3; i++)
-   {
-       cout << data[i].name << data[i].ratings[0] << data[i].ratings[1] << endl; // выводим нашу структуру
-   }
-
-   assignStatus (*data, 3);
-
-
-
-
-//   std::array<int,2> A = {5,3};
-//   std::array<int,2> B = A; //copy array A into array B
-//   students data[3];
-//   data[0].name = "Vasya";
-//   data[1].name = "Petya";
-//   data[2].name = "Sasha";
-//   data[0].ratings = {5, 4}; ошибка - http://stackoverflow.com/questions/21669818/assigning-values-to-an-array-from-an-initializer-list
-
 }
